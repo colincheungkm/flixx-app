e8df5cb2b8eb263e5f8fac123f32cadbe2088e6e
colincheungkm <colincheung1993hk@gmail.com>
2023-12-06 10:42:33 -0500

    finished movie-details

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display Popular Movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData("movie/popular");
 
@@ -38,11 +39,116 @@ async function displayPopularMovies() {
   });
 }
 
-// Fetch data from TMDB API
+// Display Popular TV shows
+async function displayPopularTV() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((tvShow) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="tv-details.html?id=${tvShow.id}">
+        ${
+          tvShow.poster_path
+            ? `<img
+          src="https://image.tmdb.org/t/p/w500${tvShow.poster_path}"
+          class="card-img-top"
+          alt="${tvShow.name}"
+        />`
+            : //   ternary operator
+              `<img
+        src="../images/no-image.jpg"
+        class="card-img-top"
+        alt="${tvShow.name}"
+      />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${tvShow.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Air Date: ${tvShow.first_air_date}</small>
+        </p>
+      </div>
+    `;
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split("=")[1];
+
+  const movie = await fetchAPIData(`movie/${movieId}`);
+  const div = document.createElement("div");
+
+  div.innerHTML = `
+  <div class="details-top">
+    <div>
+    ${
+      movie.poster_path
+        ? `<img
+      src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+      class="card-img-top"
+      alt="${movie.title}"
+    />`
+        : `<img
+    src="../images/no-image.jpg"
+    class="card-img-top"
+    alt="${movie.title}"
+  />`
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)}/ 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+        ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+       ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+      </ul>
+      <a href="${
+        movie.homepage
+      }" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+  <h2>Movie Info</h2>
+  <ul>
+    <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+      movie.budget
+    )}</li>
+    <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+      movie.revenue
+    )}</li>
+    <li><span class="text-secondary">Runtime:</span> ${
+      movie.runtime
+    } minutes</li>
+    <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">
+    ${movie.production_companies
+      .map((company) => `<span>${company.name}</span>`)
+      .join(", ")}
+  </div>
+  </div>`;
+
+  document.querySelector("#movie-details").appendChild(div);
+}
+
+// "Fetch" data from TMDB API
 async function fetchAPIData(endpoint) {
   // never do this on PROD level code - put it in a .env file
   const API_KEY = "2ea6d022b3998cbae02a76ba44dd070d";
   const API_URL = "https://api.themoviedb.org/3";
+
+  showSpinner();
 
   const response = await fetch(
     `${API_URL}/${endpoint}?api_key=${API_KEY}&language=en-US`
@@ -50,7 +156,17 @@ async function fetchAPIData(endpoint) {
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+// Spinners
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 // Highlight active NAV links
@@ -63,7 +179,11 @@ function highlightActiveLink() {
   });
 }
 
-// Initialize App
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Initialize WebApp
 function init() {
   switch (global.currentPage) {
     case "/":
@@ -73,9 +193,11 @@ function init() {
       break;
     case "/shows.html":
       console.log("Shows");
+      displayPopularTV();
       break;
     case "/movie-details.html":
       console.log("Movies Details");
+      displayMovieDetails();
       break;
     case "/tv-details.html":
       console.log("TV Details");
